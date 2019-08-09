3a6db899841569d10db3b4f4f3e544b28e15b56f
Elvis Oliveira <elvis.olv@gmail.com>
2019-08-09 10:12:42 -0300

    Hello World!

---
+++ ParseHelpers.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ConsoleApp
+{
+    internal static class ParseHelpers
+    {
+        private static Stream ToStream(this string @this)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(@this);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+        public static T Deserialize<T>(this string @this) where T : class
+        {
+            var reader = XmlReader.Create(@this.Trim().ToStream(),
+                new XmlReaderSettings()
+                {
+                    ConformanceLevel = ConformanceLevel.Document
+                }
+            );
+            return new XmlSerializer(typeof(T)).Deserialize(reader) as T;
+        }
+
+        public static string Serialize<T>(this T value)
+        {
+            if (value == null)
+            {
+                return string.Empty;
+            }
+
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            var stringWriter = new StringWriter();
+            using (var writer = XmlWriter.Create(stringWriter))
+            {
+                xmlSerializer.Serialize(writer, value);
+                return stringWriter.ToString();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp
+{
+	internal static class Program
+	{
+		private static void Main(string[] args)
+		{
+			var xml = File.ReadAllText(@"D:\NFSE.xml");
+			var xmlAsObject = xml.Deserialize<nfeProc>();
+			var objectAsXml = xmlAsObject.Serialize<nfeProc>();
+			Console.WriteLine(objectAsXml);
+		}
+	}	
+}
